50f24c0eb17b19af7c92719875a9f901b1ae7381
Oleksii <zlukapraporr@gmail.com>
2025-10-05 18:39:49 +0300

    Initial commit

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,48 @@
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AccountTest {
+
+    @Test
+    public void testBankchargePremiumLessThanAWeek() {
+        Account account = getPremiumAccount(5);
+        assertThat(account.bankcharge(), is(14.5));
+    }
+
+    @Test
+    public void testBankchargePremiumMoreThanAWeek() {
+        Account account = getPremiumAccount(9);
+        assertThat(account.bankcharge(), is(16.5));
+    }
+
+    @Test
+    public void testOverdraftFeePremium() {
+        Account account = getPremiumAccount(9);
+        assertThat(account.overdraftFee(), is(0.10));
+    }
+
+    @Test
+    public void testOverdraftFeeNotPremium() {
+        Account account = getNormalAccount();
+        assertThat(account.overdraftFee(), is(0.20));
+    }
+
+    @Test
+    public void testPrintCustomer() {
+        Account account = getNormalAccount();
+        Customer customer = new Customer("xxx", "xxx", "xxx@mail.com", CustomerType.PERSON, account);
+        account.setCustomer(customer);
+        assertThat(account.printCustomer(), is("xxx xxx@mail.com"));
+    }
+
+    private Account getNormalAccount() {
+        AccountType premium = new AccountType(false);
+        return new Account(premium, 9);
+    }
+
+    private Account getPremiumAccount(int daysOverdrawn) {
+        AccountType normal = new AccountType(true);
+        return new Account(normal, daysOverdrawn);
+    }
+}
